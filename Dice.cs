b1a698cf00464d2b5fa1e6e4ba0befcf0ac1f82f
aforbush1 <aforbush17@gmail.com>
2024-01-17 19:37:33 -0700

    Add project files.

---
+++ Dice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission2
+{
+    // Declaration of the Dice class
+    internal class Dice
+    {
+        // This object is used to generate random numbers, simulating dice rolls.
+        Random random = new Random();
+        
+        // This is the declaration of the RollDice method which is public so it can be viewed outside the class.
+        public int[] RollDice(int totalRolls)
+        {
+            int die1 = 0;
+            int die2 = 0;
+            int total = 0;
+
+            int[] totalCount = new int[11];
+
+            // FOR loop to generate and calculate the dice roll that we are simulating.
+            for (int iCount = 0; iCount < totalRolls; iCount++)
+            {
+                // Roll the two dice
+                die1 = random.Next(1, 7);
+                die2 = random.Next(1, 7);
+                total = die1 + die2;
+
+                totalCount[total - 2]++;
+            }
+
+            // After the loop completes, the method returns the 'totalCount' array, which contains the count
+            // of each possible sum from the dice rolls.
+            return totalCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Author: Adam Forbush
+// Description: Program file for dice rolling assignment
+using mission2;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        // Here I am instantiating the object
+        Dice d = new Dice();
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!" + "\n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?" + "\n");
+
+        // Ask user how many times they want to roll the dice
+        int totalRolls = int.Parse(Console.ReadLine() ?? "0");
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each " + "*" + " represents 1% of the total number of rolls.\n");
+
+        // I am calling the RollDice method from the Dice class and passing totalRolls as a parameter
+        // the totalRolls is being caught by the rollCounts array.
+        int[] rollCounts = d.RollDice(totalRolls);
+
+        // FOR loop to process the percentage and asterisks
+        for (int i = 2; i < 12; i++)
+        {
+            int count = rollCounts[i - 2];
+            // percentage = how many times the specific number was rolled / total number of rolls * 100
+            int percentage = count * 100 / totalRolls;
+            string asterisks = new string('*', percentage);
+            System.Console.WriteLine($"{i}: {asterisks}");
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
